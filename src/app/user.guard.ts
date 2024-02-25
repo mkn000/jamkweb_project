f28f02381b057d73e70a54e3c305a9c8945ea42c
Mikko Nurmiranta <n2723@student.jamk.fi>
2024-02-25 17:02:48 +0200

    angular v16

--- src/app/user.guard.ts
+++ src/app/user.guard.ts
@@ -1,12 +1,12 @@
 import {Injectable} from '@angular/core';
-import {CanActivate} from '@angular/router';
+
 import {Router} from '@angular/router';
 import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuard implements CanActivate {
+export class UserGuard  {
   constructor(private router: Router, private auth: AuthService) {}
   canActivate() {
     if (this.auth.token) {
